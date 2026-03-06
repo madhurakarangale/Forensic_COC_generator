0c9fae344c273e5ce0e32af243918853954fcaa0
Madhura Karangale <madhurakarangale03@gmail.com>
2026-03-06 22:24:17 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// Supabase configuration
+const SUPABASE_CONFIG  ={ 
+    url: 'https://fymfdydiyrpmyzuojjwy.supabase.co',
+    anonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5bWZkeWRpeXJwbXl6dW9qand5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMDkzODQsImV4cCI6MjA4Nzc4NTM4NH0.58Pm18Qrm5JQ5oxIIyJ6bDQ3LAB2ihEXAXXNsOMa3gw'
+}; 
+window.SUPABASE_CONFIG = SUPABASE_CONFIG;
